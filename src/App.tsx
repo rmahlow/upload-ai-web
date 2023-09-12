0741ac55f52cde9d76688b6d3c965df8359647e5
rmahlow <rmahlow@gmail.com>
2023-09-11 22:50:55 -0300

    frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,133 @@
-
+import { FileVideo, Github, Upload, Wand2 } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
 
 export function App() {
-
   return (
-    <h1>Hello Word</h1>
-  )
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido por Rodrigo Mahlow
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Githib
+          </Button>
+        </div>
+      </div>
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              placeholder="Inclua o promt para a IA...."
+              className="resize-none p-4 leading-relaxed"
+            />
+            <Textarea
+              placeholder="Resultado gerardo pela IA"
+              readOnly
+              className="resize-none p-4 leading-relaxed"
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: Você pode utlizar a variavel{" "}
+            <code className="text-violet-400">{"{trancription}"} </code>no seu
+            prompt para adicionar o conteúdo
+          </p>
+        </div>
+        <aside className="w-80 space-y-6">
+          <form action="" className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4" />
+              Selecione um Video
+            </label>
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+            <Separator />
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">
+                Prompt de Transcrição
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 leading-relaxed"
+                placeholder="Inclua palavras chaves mencionadas no video separadas por virgula (,)"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar Video <Upload className="w-4 h-4 ml-2" />{" "}
+            </Button>
+          </form>
+          <Separator />
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue  placeholder="Selecione um Prompt..."/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Título do YouTube</SelectItem>
+                  <SelectItem value="description">Descrição do YouTube</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+            <Separator />
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select disabled defaultValue="gpt-3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt-3.5">GPT 3.5-Turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa opção em breve
+              </span>
+            </div>
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altes tendem a deixar o resultado mais criativo.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar <Wand2 className="w-4 h-4 ml-4" />
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
+  );
 }
